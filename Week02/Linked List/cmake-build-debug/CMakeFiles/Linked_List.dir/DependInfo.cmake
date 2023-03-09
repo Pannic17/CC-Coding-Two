f9b25c1caad1eb4ce1370ffb9cbb7d7413da3893
pannic <pannic1984@outlook.com>
2023-03-09 15:22:45 +0000

    1

---
+++ Week02/Linked List/cmake-build-debug/CMakeFiles/Linked_List.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/pannic/Documents/GitHub/CC-Coding-Two/Week02/Linked List/LinkedList.cpp" "CMakeFiles/Linked_List.dir/LinkedList.cpp.o" "gcc" "CMakeFiles/Linked_List.dir/LinkedList.cpp.o.d"
+  "/Users/pannic/Documents/GitHub/CC-Coding-Two/Week02/Linked List/main.cpp" "CMakeFiles/Linked_List.dir/main.cpp.o" "gcc" "CMakeFiles/Linked_List.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Week02/Linked List/cmake-build-debug/CMakeFiles/Linked_List.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Linked_List.dir/LinkedList.cpp.o"
+  "CMakeFiles/Linked_List.dir/LinkedList.cpp.o.d"
+  "CMakeFiles/Linked_List.dir/main.cpp.o"
+  "CMakeFiles/Linked_List.dir/main.cpp.o.d"
+  "Linked_List"
+  "Linked_List.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Linked_List.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
